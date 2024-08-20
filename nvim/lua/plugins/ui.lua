608849c2834366546356f2db35dc1c549a5c7188
zumi <aponmaru@gmail.com>
2024-08-21 08:14:15 +0900

    modify: indent-blankline -> hlchunk に変更

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -31,6 +31,8 @@ return {
       })
     end
   },
+
+  -- タブラインの表示
   {
     'akinsho/bufferline.nvim',
     version = "*",
@@ -38,6 +40,34 @@ return {
     init = function ()
       vim.opt.termguicolors = true
       require("bufferline").setup{}
+    end
+  },
+
+  -- インデントガイドライン
+  {
+    "shellRaining/hlchunk.nvim",
+    event = { "UIEnter" },
+    config = function()
+      require("hlchunk").setup({
+        chunk = {
+          enable = true,
+          use_treesitter = true,
+          chars = {
+            horizontal_line = "━",
+            vertical_line = "┃",
+            left_top = "┏",
+            left_bottom = "┗",
+            right_arrow = "━",
+          },
+        },
+        blank = {
+          enable = true,
+        },
+        line_num = {
+          enable = true,
+          use_treesitter = true,
+        },
+      })
     end
   },
 }
