235d3b9cd32462660f9b598378796dc625c7d247
zumi <aponmaru@gmail.com>
2024-08-22 00:01:19 +0900

    modify: bufferline のキーマップ設定

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -36,11 +36,20 @@ return {
   {
     'akinsho/bufferline.nvim',
     version = "*",
+    event = "VeryLazy",
     dependencies = 'nvim-tree/nvim-web-devicons',
-    init = function ()
-      vim.opt.termguicolors = true
-      require("bufferline").setup{}
-    end
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        -- separator_style = "slant",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
   },
 
   -- インデントガイドライン
