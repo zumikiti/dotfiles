e545ddfce7416877b9b4fa0c4b7d828966d8830d
zumikiti <aponmaru@gmail.com>
2025-04-20 23:39:56 +0900

    add: gen.nvim with ollama

---
+++ nvim/lua/plugins/gen.lua
@@ -0,0 +1,58 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  version = false, -- Never set this value to "*"! Never!
+  opts = {
+    provider = "ollama",
+    ollama = { -- <- openai から ollama に変更
+      endpoint = "http://localhost:11434", -- Ollama の正しいエンドポイントに修正 (通常は http)
+      model = "gemma3", -- 使用したい Ollama モデル名
+      timeout = 30000,
+      temperature = 0,
+      max_completion_tokens = 8192,
+      -- その他の Ollama 固有の設定があればここに追加
+    },
+    -- openai の設定は不要であれば削除するか、必要であれば別途残します
+    -- openai = { ... },
+  },
+  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  build = "make",
+  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    --- The below dependencies are optional,
+    "echasnovski/mini.pick", -- for file_selector provider mini.pick
+    "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+    "ibhagwan/fzf-lua", -- for file_selector provider fzf
+    "echasnovski/mini.icons",
+    {
+      -- support for image pasting
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        -- recommended settings
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          -- required for Windows users
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      -- Make sure to set this up properly if you have lazy=true
+      'MeanderingProgrammer/render-markdown.nvim',
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
